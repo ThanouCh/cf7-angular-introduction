0c10877d5bf9346ceb61c80fa323ad8e3891089e
Christina Thanou <thn.xristina@gmail.com>
2025-04-17 18:18:34 +0300

    New components person table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PersonTableComponent} from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Christina';
+
+  person = {
+    givenName: 'Christina',
+    surName: 'Thanou',
+    age:29,
+    email:'thn.xristina@aueb.gr'
+  }
 }
